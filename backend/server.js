9cd55e10feee68c9b029574928f1d8e7c30d3bbe
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2024-10-23 15:52:28 +0300

    to fetch data uing api "GET"

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,30 @@ const app = express();
 //   res.send("server is ready");
 // });
 app.use(express.json());
+
+app.get("/api/products" ,async (req,res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json({
+            success:true,
+            data:products
+            
+        })
+
+        
+    } catch (error) {
+        console.log("error in fetching products ",error.message)
+        res.status(500).json({
+            success:false,
+            message:"server error"
+
+
+        })
+        
+    }
+
+})
+
 app.post("/api/products", async (req, res) => {
   const product = req.body;
   if (!product.name || !product.price || !product.image) {
@@ -30,13 +54,23 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
-app.delete("api/products/:id"  , async (req, res) => {
-    const {id} = req.params
-
-
-
-})
-
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+    });
+  } catch (error) {
+    console.log("error deleting the  product", error.message);
+    //res
+    res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+});
 
 app.listen(5000, () => {
   connectDb();
